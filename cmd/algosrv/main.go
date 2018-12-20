23a05c72f9a405e94b43e1ec24b57abfa3880114
fanghr <chfanghr@gmail.com>
2018-12-21 01:18:26 +0800

    Update

---
+++ cmd/algosrv/main.go
@@ -0,0 +1,20 @@
+package algosrv
+
+import (
+	"log"
+	"os"
+)
+
+var Cleanfuncs *CleanUpHandlerArray
+var Logger *log.Logger
+
+func main() {
+	Logger = log.New(os.Stdout, "", log.LstdFlags)
+	Cleanfuncs = NewCleanUpHandlerArray(Logger)
+
+	defer func() {
+		if Cleanfuncs != nil {
+			Cleanfuncs.Wait()
+		}
+	}()
+}
